537ede3e32978297b68df1dc1f0f7dd240c0eda2
li.haojie01 <li.haojie01@iwhalecloud.com>
2021-01-23 17:47:19 +0800

    责任链模式01

---
+++ behavioral-pattern/src/main/java/com/lee/responsibility_chain/demo01/FileLogger.java
@@ -0,0 +1,13 @@
+package com.lee.responsibility_chain.demo01;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
---
+++ behavioral-pattern/src/main/java/com/lee/responsibility_chain/demo01/TestDemo.java
@@ -0,0 +1,25 @@
+package com.lee.responsibility_chain.demo01;
+
+/**
+ * 测试类，可以进一步封装抽象，暴露给外部的只有艺哥日志处理类，内部逻辑不有由用户实现
+ */
+public class TestDemo {
+    private static AbstractLogger getChainOfLoggers(){
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+    }
+}
